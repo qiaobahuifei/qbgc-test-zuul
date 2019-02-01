8bbb371a697730e8440089ff5a92fb282a3dd162
gaochao <gaochao5kuba@163,com>
2019-02-01 14:01:54 +0800

    first

---
+++ src/main/java/com/qbgc/test/zuul/filter/MyFilter.java
@@ -0,0 +1,73 @@
+package com.qbgc.test.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+
+    /**
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     *
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤的顺序
+     *
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 这里可以写逻辑判断，是否要过滤，true为永远过滤
+     *
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
+     *
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        // 获取当前上下文
+        RequestContext context = RequestContext.getCurrentContext();
+        // 获取request
+        HttpServletRequest request = context.getRequest();
+        Object token = request.getParameter("token");
+        if (token == null) {
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                context.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        return null;
+    }
+}
